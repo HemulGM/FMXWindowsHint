3a6c92889c4e4ef24f44d558bb641a9a50cce424
HemulGM <alinvip22@gmail.com>
2024-10-22 12:47:46 +0500

    Initial commit

---
+++ FMX.Windows.Hints.pas
@@ -0,0 +1,54 @@
+﻿unit FMX.Windows.Hints;
+
+interface
+
+uses
+  FMX.Controls, FMX.Forms;
+
+type
+  TCustomHint = class(THint)
+  protected
+    procedure SetEnabled(const Value: Boolean); override;
+  public
+    procedure SetPosition(const X, Y: Single); override;
+    procedure SetHint(const AString: string); override;
+  end;
+
+  THintHelper = class helper for THint
+    class procedure Clear;
+  end;
+
+implementation
+
+{ THintHelper }
+
+class procedure THintHelper.Clear;
+begin
+  with Self do
+    FClassRegistry := [];
+end;
+
+{ TMyHint }
+
+procedure TCustomHint.SetEnabled(const Value: Boolean);
+begin
+  inherited;
+end;
+
+procedure TCustomHint.SetHint(const AString: string);
+begin
+  inherited;
+end;
+
+procedure TCustomHint.SetPosition(const X, Y: Single);
+begin
+  inherited;
+end;
+
+initialization
+  THint.Clear;
+  THint.RegisterClass(TCustomHint);
+  Application.OnHint := nil;
+
+end.
+
